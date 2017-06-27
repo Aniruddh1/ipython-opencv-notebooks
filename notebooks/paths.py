5ecf930c2cdb508a2a125fcb4cc316963bf2464f
Tom Rafferty <trafferty@cnt.canon.com>
2017-06-27 08:37:33 -0500

    added paths.py helper to allow easier linking to subdirs from notebooks at any point in tree

---
+++ notebooks/paths.py
@@ -0,0 +1,10 @@
+import os
+
+data_root = os.environ['DATA_ROOT']
+notebook_root = "%s/notebooks" % (data_root)
+scripts_root = "%s/scripts" % (data_root)
+images_root = "%s/images" % (data_root)
+projects_root = "%s/projects" % (data_root)
+results_root = "%s/results" % (data_root)
+tmp_dir = "%s/tmp" % (data_root)
+
