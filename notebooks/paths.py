e258a7f9e4237907e39c29391b8d90d113c628a1
Tom Rafferty <trafferty@cnt.canon.com>
2017-10-13 11:17:33 -0500

    Added notebooke for analyzing TTM scores and plotting to wafer layout

--- notebooks/paths.py
+++ notebooks/paths.py
@@ -6,7 +6,11 @@ scripts_root = "%s/scripts" % (data_root)
 images_root = "%s/images" % (data_root)
 projects_root = "%s/projects" % (data_root)
 results_root = "%s/results" % (data_root)
-tmp_dir = "%s/tmp" % (data_root)
+tmp_root = "%s/tmp" % (data_root)
+
+from os.path import expanduser
+tmp_dir = '%s/tmp' % (expanduser("~"))
+ws_dir = '%s/workspace' % (expanduser("~"))
 
 path = {}
 path['notebook_root']=notebook_root
@@ -14,4 +18,6 @@ path['scripts_root']=scripts_root
 path['images_root']=images_root
 path['projects_root']=projects_root
 path['results_root']=results_root
+path['tmp_root']=tmp_root
 path['tmp_dir']=tmp_dir
+path['ws_dir']=ws_dir
